249e55526f8e672c66eebbeb4f24ee4d89745c7a
InYourG00D1 <44290386+InYourG00D1@users.noreply.github.com>
2019-02-23 15:06:12 +0700

    Add files via upload

---
+++ KD-FB.sh
@@ -0,0 +1,381 @@
+#!/system/xbin/bash
+#Jangan di recode dong ster 
+#usaha kalo gk bisa nanya 
+
+
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+
+echo "\033[0;32m]
+      .---.        .-----------
+     /     \  __  /    ------
+    / /     \(oo)/    -----
+   //////   * \/ *   ---
+  //// / // :    : ---
+ // /   /  /*    *--
+//          //..\\
++==========UU====UU============+
++          *//||\\*            +
++           ****               +
++ coded by : InYourG00D        +
++ Team : LNX#CREW S.T.C BUFT   +
++ kontak me : 6281382406878    +
++==============================+
+  ^...^                  ^...^  
+ <_* *_>KumpulanKodeUnik<_* *_> 
+   \_/                    \_/   ";
+echo $blue "MenuKode"
+echo $green "[====================================[>"
+echo "1.SimpulCinta"|lolcat
+echo $red "[====================================[>"
+echo "2.GambarKeren"|lolcat
+echo $yellow "[====================================[>"
+echo "3.StatusPanjang"|lolcat
+echo $green "[====================================[>"
+echo "4.Status cinta & Lucu"|lolcat
+echo $yellow "[====================================[>"
+echo "5.Fuck"|lolcat
+echo $blue "[====================================[>"
+echo "6.Rumah"|lolcat
+echo $yellow "[====================================[>"
+echo "7.UcapSalam"|lolcat
+echo $cyan "[====================================[>"
+echo "8.Gitar"|lolcat
+echo $yellow "[====================================[>"
+echo "9.Kepala babi"|lolcat
+echo $red "[====================================[>"
+echo "10.wajah cowok"|lolcat
+echo $yellow "[====================================[>"
+echo "11.Rumah bertingkat"|lolcat
+echo $green "[====================================[>"
+echo "12.Love"|lolcat
+echo $yellow "[====================================[>"
+echo "13.Bergambar Unik"|lolcat
+echo $blue "[====================================[>"
+echo "14.Status Pake Gambar"|lolcat
+echo $yellow "[====================================[>"
+echo "15.Status Cinta"|lolcat
+echo $cyan "[====================================[>"
+echo "16.NGANCEM"|lolcat
+echo $yellow "[====================================[>"
+echo "17.GambarBinatang"|lolcat
+echo $red "[====================================]>"
+echo "18.PuisiKepergian"|lolcat
+echo $purple "[====================================]>"
+echo "19.Kata2 Bucin"|lolcat
+echo $green "[====================================]>"
+echo "20.Status Rindu(Bucin)"|lolcat
+echo $blue "[====================================]>"
+echo "21.Komentar Panjang"|lolcat
+echo $red "[====================================]>"
+echo "22.UlangTahun"|lolcat
+echo $yellow "[====================================]>"
+echo "23.KataGombal"|lolcat
+echo $cyan "[====================================]>"
+echo "24.Gambar"|lolcat
+echo $blue "[====================================]>"
+echo "25.Kata2 Terbalik"|lolcat
+echo $red "[====================================]>"
+echo "26.Like Maksa"|lolcat
+echo $purple "[====================================]>"
+echo "27.Kode Lucu"|lolcat
+echo $blue "[====================================]>"
+echo "28.SenyumSenyumSendiri(OrgGila)"|lolcat
+echo $red "[====================================]>"
+echo "00.Out"|lolcat
+echo $red
+read -p "[root@IndoDark~#" rgl
+
+if [ $rgl = 1 ] || [ $rgl = 1 ]
+then
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > @[333484123400584:]"
+fi
+
+if [ $rgl = 2 ] || [ $rgl = 2 ]
+then
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > @[321482381283489:]
+@[190907441044255:]
+@[274043292712053:]
+@[303767899730286:]
+@[154078458065724:]
+@[357099854373849:]
+@[284415944997837:]
+@[467810786572658:]
+@[406332776098692:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 3 ] || [ $rgl = 3 ]
+then 
+clear 
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > @[187747418027575:]
+@[277686315681671:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 4 ] || [ $rgl = 4 ]
+then 
+clear 
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "Kode nya > @[150574045083762:]
+@[483537345004355:]
+@[451135108262570:]
+@[490365987663518:]
+@[124649934357807:]
+@[124240437733139:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 5 ] || [ $rgl = 5 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > @[358838604176633:]"
+echo "-======DONE======-"
+fi 
+
+if [ $rgl = 6 ] || [ $rgl = 6 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[138802602865272:]
+@[133739576643141:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 7 ] || [ $rgl = 7 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[147382575371282:]
+@[249129151821573:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 8 ] || [ $rgl = 8 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[173009899510754:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 9 ] || [ $rgl = 9 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[283729368393272:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 10 ] || [ $rgl = 10 ]
+then 
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[285334424902197:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 11 ] || [ $rgl = 11 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[321774641246891:]"
+fi
+
+if [ $rgl = 12 ] || [ $rgl = 12 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[357099854373849:]
+@[284414614913997:]
+@[137478143021146:]
+@[225634690832025:]
+@[347609151971197:]
+@[446435198740215:]
+@[301823366586418:]"
+fi
+
+if [ $rgl = 13 ] || [ $rgl = 13 ]
+then 
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[251620911614956:]"
+fi
+
+if [ $rgl = 14 ] || [ $rgl = 14 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[467743819935765:]
+@[494817327218130:]"
+fi
+
+if [ $rgl = 15 ] || [ $rgl = 15 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[474704252540326:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 16 ] || [ $rgl = 16 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > "
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 17 ] || [ $rgl = 17 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[337548616270115:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 18 ] || [ $rgl = 18 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[308039315964393:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 19 ] || [ $rgl = 19 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[298799600231957:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 20 ] || [ $rgl = 20 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[193452200784660:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 21 ] || [ $rgl = 21 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[312301218885168:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 22 ] || [ $rgl = 22 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[277233245642912:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 23 ] || [ $rgl = 23 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[413484778707007:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 24 ] || [ $rgl = 24 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[414389335247765:]
+@[386136058111515:]
+@[377030165711320:]
+@[137983256348474:]
+@[154078458065724:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 25 ] || [ $rgl = 25 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[318379914925133:]
+@[332669410160042:]
+@[435190793189544:]
+@[351438108264336:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 26 ] || [ $rgl = 26 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[251122141673598:]
+@[393990440662113:]
+@[397112680349645:]
+@[341536905931308:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 27 ] || [ $rgl = 27 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > 
+@[1948049839361:]
+@[100000834121428:]
+@[100001675120071:]
+@[112172868798181:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 28 ] || [ $rgl = 28 ]
+then 
+clear
+echo "Tinggal Coppy Aja Gan"| lolcat
+echo "kode nya > @[161945097284850:]
+@[535659356463566:]
+@[496668933688445:]
+@[336100766487119:]
+@[225098077621600:]"
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $rgl = 00 ] || [ $rgl = 00 ]
+then 
+clear
+echo "Tq Udh install Tools ini"| lolcat
+echo "Support LNX#CREW"|lolcat
+echo "Btw Gw Singgle Loh Kuy Lah"|lolcat
+echo "Nomer Wa : 6281382406878"|lolcat
+exit
+echo "-=====DONE=====-"|lolcat
+fi
